b6ba3495557f8d1e7d620723acac52fba8850c95
SaikumarK1-hexaware <1000071583@hexaware.com>
2023-02-24 05:31:09 +0000

    pushing springboot mongodb mra

---
+++ SprinBootMdbMRA/src/main/java/com/SprinBootMdbMRA/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.SprinBootMdbMRA.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	public EntityNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
